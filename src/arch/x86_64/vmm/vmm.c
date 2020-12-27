85a6494381885c266708e0cf01e5f5fa0a17e66f
MRNIU <a6813140@hotmail.com>
2020-12-27 18:54:23 +0800

    pref(vmm): 规范代码
    Signed-off-by: MRNIU <a6813140@hotmail.com>

--- src/arch/x86_64/vmm/vmm.c
+++ src/arch/x86_64/vmm/vmm.c
@@ -23,41 +23,40 @@ pte_t pte_kernel[VMM_PAGE_TABLES_KERNEL][VMM_PAGES_PRE_PAGE_TABLE]
 
 // TODO: 完善缺页处理
 void page_fault(pt_regs_t *pt_regs) {
+    ptr_t pg_addr = 0x00;
 #ifdef __x86_64__
-    uint64_t cr2;
-    __asm__ volatile("movq %%cr2,%0" : "=r"(cr2));
+    __asm__ volatile("movq %%cr2,%0" : "=r"(pg_addr));
 #else
-    uint32_t cr2;
-    __asm__ volatile("mov %%cr2,%0" : "=r"(cr2));
+    __asm__ volatile("mov %%cr2,%0" : "=r"(pg_addr));
 #endif
-    printk("Page fault at 0x%08X, virtual faulting address 0x%08X\n",
-           pt_regs->eip, cr2);
-    printk_err("Error code: 0x%08X\n", pt_regs->err_code);
+    printk("Page fault at 0x%X, virtual faulting address 0x%X\n", pt_regs->eip,
+           pg_addr);
+    printk_err("Error code: 0x%X\n", pt_regs->err_code);
 
     // bit 0 为 0 指页面不存在内存里
-    if (!(pt_regs->err_code & 0x1)) {
-        printk_color(red, "Because the page wasn't present.\n");
+    if ((pt_regs->err_code & 0x1) == 0x1) {
+        printk_err("Page wasn't present.\n");
     }
     // bit 1 为 0 表示读错误，为 1 为写错误
-    if (pt_regs->err_code & 0x2) {
+    if ((pt_regs->err_code & 0x2) == 0x2) {
         printk_err("Write error.\n");
     }
     else {
         printk_err("Read error.\n");
     }
-    // bit 2 为 1 表示在用户模式打断的，为 0 是在内核模式打断的
-    if (pt_regs->err_code & 0x4) {
+    // bit 2 为 0 是在内核模式打断的，为 1 表示在用户模式打断的，
+    if ((pt_regs->err_code & 0x4) == 0x4) {
         printk_err("In user mode.\n");
     }
     else {
         printk_err("In kernel mode.\n");
     }
     // bit 3 为 1 表示错误是由保留位覆盖造成的
-    if (pt_regs->err_code & 0x8) {
+    if ((pt_regs->err_code & 0x8) == 0x8) {
         printk_err("Reserved bits being overwritten.\n");
     }
     // bit 4 为 1 表示错误发生在取指令的时候
-    if (pt_regs->err_code & 0x10) {
+    if ((pt_regs->err_code & 0x10) == 0x10) {
         printk_err("The fault occurred during an instruction fetch.\n");
     }
     while (1) {
